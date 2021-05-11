358ec789bf5b371abc3ab23292adc21e74dc60bf
Sebastian Sanchez <sebastian@artxcode.io>
2021-05-11 17:41:48 -0400

    working on splash page

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import ProductIndexContainer from '../products/product_index_container';
+import FeaturedImagesContainer from './featured_images_container';
+
+class Splash extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+
+        return (
+
+        );
+
+    }
+}
+
+export default Splash;
