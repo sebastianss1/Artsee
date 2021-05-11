1054b7dd231792ea8f69a986057b58256eb188c3
Sebastian Sanchez <sebastian@artxcode.io>
2021-05-11 13:25:26 -0400

    heroku setup

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,6 +1,6 @@
 class Artwork < ApplicationRecord 
     validates :title, :year, :artist_id, :description, :height_dimensions, :width_dimensions, :medium_id, :availability, :price, presence: true
-    validates [:title, :artist_id], uniqueness: true 
+    validates :title, uniqueness: { scope: :artist_id } 
 
     belongs_to :artist,
     foreign_key: :artist_id,
