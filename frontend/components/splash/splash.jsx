9a188831d5b9daa55e8d4b3ded8de5ae726443ea
Sebastian Sanchez <sebastian@artxcode.io>
2021-05-12 20:58:40 -0400

    styled hero image and text

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -11,22 +11,26 @@ class Splash extends React.Component {
 
         return (
             <div className="hero-container">
-            <div className='hero'>
                 <img className="banner-image" src={window.banner_image} />
+                <div className='hero'>
                 <div className='titles'>
                     <h1 className='hero-text'>Learn about and collect art from leading galleries, fairs, and museums</h1>
                         <h5 className='hero-text-subhead'>Sign up to get updates on your favorite artists</h5> <br /><br /><br /><br />
-                        <h3>Trending Emerging Artists</h3>
-                        <p>This week, Artsy’s Curatorial team has kept our fingers on the pulse, taking note of the most popular artists currently on the scene. 
-                            <br />From innovative sculptors to experimental photographers, here’s a chance to explore the trailblazing artists turning heads in the art world.</p>
+                        <button className="banner-signup"> Sign Up</button>
                 </div>
             </div>
             </div>
 
-        );
+);
 
-    }
+}
 }
 
 export default Splash;
 
+
+{/* <div className="">
+        <h3>Trending Emerging Artists</h3>
+        <p>This week, Artsy’s Curatorial team has kept our fingers on the pulse, taking note of the most popular artists currently on the scene. 
+            <br />From innovative sculptors to experimental photographers, here’s a chance to explore the trailblazing artists turning heads in the art world.</p>
+</div> */}
